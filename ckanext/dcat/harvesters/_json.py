ca551606b8df00aff61f9971d636457f90c72b2d
vaquer <akura11.tt@gmail.com>
2017-12-12 12:40:40 -0600

    Test(log): Mejora el logueo desde el base del harvest sin errores

--- ckanext/dcat/harvesters/_json.py
+++ ckanext/dcat/harvesters/_json.py
@@ -49,7 +49,7 @@ class DCATJSONHarvester(DCATHarvester):
 
         dcat_dict = json.loads(content)
 
-        log.debug("Vocabulario {0}".format(self.vocabulary)
+        log.debug("Vocabulario {0}".format(self.vocabulary))
         package_dict = converters.dcat_to_ckan(dcat_dict, self.vocabulary)
 
         return package_dict, dcat_dict
